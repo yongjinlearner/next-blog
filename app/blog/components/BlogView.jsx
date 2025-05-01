0c1476e17f70c4282e348dc57e09ba376f216ef2
Yongjin Lee <yl2889@cornell.edu>
2025-05-01 18:04:33 +0900

    minimal changes made at work

--- app/blog/components/BlogView.jsx
+++ app/blog/components/BlogView.jsx
@@ -4,8 +4,21 @@ import Link from 'next/link';
 import Image from 'next/image';
 import { useEffect, useState } from 'react';
 
+const handleSubscription = async (formData) => {
+    'use server'
+    // Handle the form submission here
+    const email = formData.get('email')
+    console.log(email)
+}
+
 export default function BlogView() {
     return (
-        <h1>Hello world!</h1>
+        <div className='flex flex-col items-center justify-center'>
+            <form action={handleSubscription}>
+                <label>Subscribe to the blog!</label>
+                <input type='email' name='email' placeholder='Enter your email' required />
+                <button type='submit'>Subscribe</button>
+            </form>
+        </div>
     )
 }
