2ea922797f2e460958ad4a2cfc7a2952e16360c8
Yongjin Lee <yl2889@cornell.edu>
2025-06-11 23:20:12 +0900

    no errors please

--- app/confirm-email/page.js
+++ app/confirm-email/page.js
@@ -1,7 +1,7 @@
 'use client';
 
 import { useSearchParams } from 'next/navigation';
-import { confirmEmail } from '@/app/actions/email/confirmEmail';
+import { confirmEmail } from '@/actions/email/confirmEmail';
 
 export default function ConfirmEmail() {
     const searchParams = useSearchParams(); // Access query parameters
