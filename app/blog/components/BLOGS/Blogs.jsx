19f764d47f730242f7c74a5c4d3d2f8a597b50a6
ylee26 <yl2889@cornell.edu>
2025-06-16 19:28:55 +0900

    blog can load

--- app/blog/components/BLOGS/Blogs.jsx
+++ app/blog/components/BLOGS/Blogs.jsx
@@ -1,5 +1,5 @@
 'use client'
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import BlogPreview from '@/app/blog/components/BLOGS/BlogPreview';
 
 
@@ -9,11 +9,12 @@ export default function Blogs() {
     const [blogs, setBlogs] = useState([]);
 
     useEffect(() => {
-      fetch('/api/blogs')
-        .then(res => res.json())
-        .then(data =>{ 
-            console.log('Fetched blogs in component:', data)
-            setBlogs(data)});
+        fetch('/api/blogs')
+            .then(res => res.json())
+            .then(data => {
+                console.log('Fetched blogs in component:', data)
+                setBlogs(data)
+            });
     }, []);
 
     const handleLoadMore = () => {
@@ -21,18 +22,19 @@ export default function Blogs() {
     }
     const sortedBlogs = [...blogs].sort((a, b) => new Date(b.date) - new Date(a.date));
     const slicedBlogs = sortedBlogs.slice(0, Math.min(blogs.length, visibleItemCount))
-    
+
     const blogList = slicedBlogs.map((blog, index) => {
         console.log(blog._id);
         return (
-            <BlogPreview key={blog._id} index={index} blog={blog} /> 
+            <BlogPreview key={blog._id} index={index} blog={blog} />
         );
     });
-    
+
     return (
         <div className="flex flex-col gap-5 min-h-screen">
+            {!blogList.length && <p>Loading...</p>}
             {blogList}
-            {blogs.length>visibleItemCount && <button onClick={handleLoadMore} className="btn-primary">Load More</button>}
+            {blogs.length > visibleItemCount && <button onClick={handleLoadMore} className="btn-primary">Load More</button>}
         </div>
     );
 }
